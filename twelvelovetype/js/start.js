fc2c827ebefa5fcd4d997f58c875044bad7ce526
HongDawww <d939305@naver.com>
2023-11-04 22:46:32 +0900

    animation + qna

--- twelvelovetype/js/start.js
+++ twelvelovetype/js/start.js
@@ -1,10 +1,29 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
+const endPoint = 12;
 
+function goResult() {
+	qna.style.webkitAnimation = "fadeOut 1s";
+	qna.style.animation = "fadeOut 1s";
+	setTimeout(() => {
+		result.style.webkitAnimation = "fadeIn 1s";
+		result.style.animation = "fadeIn 1s";
+		setTimeout(() => {
+			qna.style.display = "none";
+			result.style.display = "block";
+
+		}, 450)})
+}
 function addAnswer(answerText, qIdx){
 	let a = document.querySelector(".answerBox");
 	let answer = document.createElement("button")
 	answer.classList.add("answerList");
+	answer.classList.add("my-3");
+	answer.classList.add("py-3");
+	answer.classList.add("mx-auto");
+	answer.classList.add("fadeIn");
+
 	a.appendChild(answer);
 	answer.innerHTML = answerText;
 
@@ -12,18 +31,32 @@ function addAnswer(answerText, qIdx){
 		let children = document.querySelectorAll(".answerList");
 		for(let i = 0; i < children.length; i++){
 			children[i].disabled = true;
-			children[i].style.display = "none";
+			children[i].style.webkitAnimation = "fadeOut 0.5s";
+			children[i].style.animation = "fadeOut 0.5s";
 		}
-		goNext(++qIdx);
+		setTimeout(() => {
+			for(let i = 0; i < children.length; i++){
+				children[i].style.display = "none";
+			}
+			goNext(++qIdx);
+		}, 450)
+	
 	}, false);
 }
 
 function goNext(qIdx){
- let q = document.querySelector(".qBox");
- q.innerHTML = qnaList[qIdx].q;
- for(let i in qnaList[qIdx].a){
-	addAnswer(qnaList[qIdx].a[i].answer,qIdx); 
- }
+	if(qIdx+1 === endPoint) {
+		goResult();
+		return;
+	}
+
+	let q = document.querySelector(".qBox");
+	q.innerHTML = qnaList[qIdx].q;
+	for(let i in qnaList[qIdx].a){
+		addAnswer(qnaList[qIdx].a[i].answer,qIdx); 
+	}
+		let status = document.querySelector('.statusBar');
+		status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
 function begin () {
