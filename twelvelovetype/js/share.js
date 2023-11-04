d60cf8bc94c8b8ab6f7487318eb7fcb9e5e61cdd
HongDawww <d939305@naver.com>
2023-11-05 01:57:41 +0900

    카카오톡 공유하기

--- twelvelovetype/js/share.js
+++ twelvelovetype/js/share.js
@@ -1,40 +1,35 @@
-function kakaoShare () {
+const url = "https://12lovetypetest12.netlify.app/";
 
-	Kakao.Share.createDefaultButton({
-		container: '#kakaotalk-sharing-btn',
+function setShare() {
+	let resultImg =  document.querySelector('#resultImg');
+	let resultAlt = resultImg.firstElementChild.alt;
+	const shareTitle = "십이간지 연애유형 결과";
+	const shareDes = infoList[resultAlt].name;
+	const shareImage = url + 'img/image-' + resultAlt + '.png';
+	const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+
+	Kakao.Link.sendDefault({
 		objectType: 'feed',
 		content: {
-		  title: '딸기 치즈 케익',
-		  description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
-		  imageUrl:
-			'http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+		  title: shareTitle,
+		  description: shareDes,
+		  imageUrl:shareImage,
 		  link: {
-			// [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-			mobileWebUrl: 'https://developers.kakao.com',
-			webUrl: 'https://developers.kakao.com',
+			mobileWebUrl: shareURL,
+			webUrl: shareURL,
 		  },
-		},
-		social: {
-		  likeCount: 286,
-		  commentCount: 45,
-		  sharedCount: 845,
 		},
 		buttons: [
 		  {
-			title: '웹으로 보기',
+			title: '결과 확인하기',
 			link: {
-			  mobileWebUrl: 'https://developers.kakao.com',
-			  webUrl: 'https://developers.kakao.com',
+			  mobileWebUrl: shareURL,
+			  webUrl: shareURL,
 			},
-		  },
-		  {
-			title: '앱으로 보기',
-			link: {
-			  mobileWebUrl: 'https://developers.kakao.com',
-			  webUrl: 'https://developers.kakao.com',
-			},
-		  },
+		  }
+		  
 		],
 	  });
 
-}
+	}
