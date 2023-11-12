1a2999d1ecf8165a7def3f080c6b8c0f308ad5a9
HongDawww <d939305@naver.com>
2023-11-12 19:07:26 +0900

    saveimg

--- canvas/index.js
+++ canvas/index.js
@@ -1,3 +1,5 @@
+const saveBtn = document.getElementById("save");
+const textInput = document.getElementById("text");
 const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
@@ -9,11 +11,14 @@ const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
-canvas.width = 800;
-canvas.height = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 let isPainting = false;
 let isFilling = false;
@@ -77,9 +82,28 @@ function onFileChange(event) {
 	image.src = url;
 	image.onload = function(){
 		ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+		fileInput.value = null;
 	};
 }
-
+function onDoubleClick(event){
+	const text = textInput.value;
+	if(text !== ""){
+		ctx.save();
+		ctx.lineWidth = 1;
+		ctx.font = "68px serif";
+		ctx.strokeText(text, event.offsetX, event.offsetY);
+		ctx.restore();
+	}
+}
+function onSaveClick() {
+	const url = canvas.toDataURL();
+	const a = document.createElement("a");
+	a.href = url;
+	a.download = "myDrawing.png";
+	a.click();
+  }
+  
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
@@ -95,3 +119,4 @@ modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click",onEraserClick);
 fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
