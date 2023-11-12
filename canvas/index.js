77233d047960eb823757bc5e3b1e60f31185cd86
HongDawww <d939305@naver.com>
2023-11-12 18:26:03 +0900

    eraser +

--- canvas/index.js
+++ canvas/index.js
@@ -1,5 +1,6 @@
-
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 const colorOptions = Array.from(
 	document.getElementsByClassName("color-option")
 );
@@ -7,6 +8,8 @@ const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
@@ -54,8 +57,17 @@ function onModeClick(){
 }
 function onCanvasClick() {
 	if(isFilling){
-		ctx.fillRect(0,0, 800, 800);
+		ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
 	}
+}
+function onDestroyClick(){
+	ctx.fillStyle = "white";
+	ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+function onEraserClick() {
+	ctx.strokeStyle = "white";
+	isFilling = false;
+	modeBtn.innerText = "Fill";
 }
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
@@ -68,4 +80,6 @@ color.addEventListener("change",onColorChange);
 colorOptions.forEach(color => color.addEventListener
 	("click",onColorClick));
 
-modeBtn.addEventListener("click", onModeClick)
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click",onEraserClick);
